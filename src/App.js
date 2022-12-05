aced617435bab803e9e946ded5c6636a2b15eac2
HughesSeanKyle <khughessean@yahoo.com>
2022-12-05 14:58:51 +0200

    Basic routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import { BrowserRouter, Navigate, Routes, Route } from 'react-router-dom';
+import HomePage from './scenes/homePage';
+import LoginPage from './scenes/loginPage';
+import ProfilePage from './scenes/profilePage';
+import { useSelector } from 'react-redux';
+
 function App() {
-	return <div className="app">Hellow</div>;
+	// const mode = useSelector((state) => state.mode);
+
+	return (
+		<div className="app">
+			<BrowserRouter>
+				<Routes>
+					<Route path="/" element={<LoginPage />} />
+					<Route path="/home" element={<HomePage />} />
+					<Route path="/profile/:userId" element={<ProfilePage />} />
+				</Routes>
+			</BrowserRouter>
+		</div>
+	);
 }
 
 export default App;
