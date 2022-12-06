c4729a7ff699fda37a301ba09f39fe95c6912efb
HughesSeanKyle <khughessean@yahoo.com>
2022-12-06 14:28:03 +0200

    Update endpoint ports and debug image retrieval

--- src/scenes/loginPage/Form.js
+++ src/scenes/loginPage/Form.js
@@ -64,7 +64,7 @@ const Form = () => {
 		formData.append('picturePath', values.picture.name);
 
 		const savedUserResponse = await fetch(
-			'https://mern-social-media-three.vercel.app/auth/register',
+			'http://localhost:3001/auth/register',
 			{
 				method: 'POST',
 				body: formData,
@@ -79,20 +79,18 @@ const Form = () => {
 	};
 
 	const login = async (values, onSubmitProps) => {
-		const loggedInResponse = await fetch(
-			'https://mern-social-media-three.vercel.app/auth/login',
-			{
-				method: 'POST',
-				headers: { 'Content-Type': 'application/json' },
-				body: JSON.stringify(values),
-			}
-		);
+		const loggedInResponse = await fetch('http://localhost:3001/auth/login', {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify(values),
+		});
 		const loggedIn = await loggedInResponse.json();
+		console.log('loggedIn', loggedIn);
 		onSubmitProps.resetForm();
 		if (loggedIn) {
 			dispatch(
 				setLogin({
-					user: loggedIn.user,
+					user: loggedIn.foundUser.data,
 					token: loggedIn.token,
 				})
 			);
